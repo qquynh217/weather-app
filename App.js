315f4a637c8b05011424b49c48e44ca0e79e1f6c
qquynh217 <nltquynh875@gmail.com>
2023-12-09 22:04:45 +0700

    update

--- App.js
+++ App.js
@@ -1,10 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import AppNavigation from './navigation/appNavigation';
-import * as Device from 'expo-device';
-import * as Notifications from 'expo-notifications';
-import Constants from 'expo-constants';
-import { useEffect } from 'react';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import AppNavigation from "./navigation/appNavigation";
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
+import Constants from "expo-constants";
+import { useEffect } from "react";
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -13,34 +13,32 @@ Notifications.setNotificationHandler({
     shouldSetBadge: false,
   }),
 });
-async function getNotificationToken(){
-  if (Platform.OS === 'android') {
-    await Notifications.setNotificationChannelAsync('default', {
-      name: 'default',
+async function getNotificationToken() {
+  if (Platform.OS === "android") {
+    await Notifications.setNotificationChannelAsync("default", {
+      name: "default",
       importance: Notifications.AndroidImportance.MAX,
       vibrationPattern: [0, 250, 250, 250],
-      lightColor: '#FF231F7C',
+      lightColor: "#FF231F7C",
     });
   }
-  const {status} = await Notifications.getPermissionsAsync()
-  if(status!='granted'){
-    const {status} = await Notifications.requestPermissionsAsync()
-    if(status!='granted'){
-      alert('failed to get notification token')
-      return
+  const { status } = await Notifications.getPermissionsAsync();
+  if (status != "granted") {
+    const { status } = await Notifications.requestPermissionsAsync();
+    if (status != "granted") {
+      alert("failed to get notification token");
+      return;
     }
   }
-  const tokenData = await Notifications.getExpoPushTokenAsync()
-  const token = tokenData.data
-  console.log({token});
-  return token
+  const tokenData = await Notifications.getExpoPushTokenAsync();
+  const token = tokenData.data;
+  console.log({ token });
+  return token;
 }
 
 export default function App() {
-  useEffect(()=>{
-    getNotificationToken()
-  },[])
-  return (
-    <AppNavigation />
-  );
+  // useEffect(()=>{
+  //   getNotificationToken()
+  // },[])
+  return <AppNavigation />;
 }
